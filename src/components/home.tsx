b4c26d78f14d3a1d579e07b729fe4aea6cab1c5f
KibromHS <kibromhs81@gmail.com>
2025-04-22 10:18:52 +0300

    init commit

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { json, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import AuthLayout from "@/components/layout/AuthLayout";
 import LoginForm from "./auth/LoginForm";
 
